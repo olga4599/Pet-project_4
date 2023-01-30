d98f15e213f6b83bca176d5bab259a066e8f4293
olga4599 <73193270+olga4599@users.noreply.github.com>
2023-01-30 20:58:31 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,18 @@
+﻿class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f'Rectangle: {self.x}, {self.y}, {self.width},{self.height}'
+
+    def find_area(self):
+        return self.width*self.height
+
+r1 = Rectangle(5,10,50,100)
+print(r1)
+print(r1.find_area())
+
+
